e34c2bff0a80e44e7db0ab1afaab060cefb84598
Bartosz Kaczkowski <bartosz.kaczkowski@pragmatists.pl>
2020-09-09 22:10:21 +0200

    made test run from beggining to the end

--- tests/src/test/java/io/sealights/gradle/tests/BuildScannerTests.java
+++ tests/src/test/java/io/sealights/gradle/tests/BuildScannerTests.java
@@ -1,8 +1,13 @@
 package io.sealights.gradle.tests;
 
-import io.sealights.onpremise.agentevents.eventservice.proxy.api.AgentEventRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.sealights.onpremise.agents.buildscanner.buildmap.service.proxy.api.BuildMappingRequest;
+import io.sealights.onpremise.agents.buildscanner.buildmap.service.proxy.api.HashedMethodData;
 import io.sealights.onpremise.agents.infra.component.tests.mockserver.DefaultMockServerAPI;
 import io.sealights.onpremise.agents.infra.component.tests.mockserver.MockServer;
+import io.sealights.onpremise.agents.infra.env.OsDetector;
+import io.sealights.onpremise.agents.infra.types.Component;
+import lombok.SneakyThrows;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -10,7 +15,14 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BuildScannerTests {
 
@@ -22,10 +34,20 @@ public class BuildScannerTests {
     private static String token;
     private static DefaultMockServerAPI mockServerAPI;
 
+    private static ObjectMapper objectMapper = getObjectMapper();
+
+    private static ObjectMapper getObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
+
     @BeforeClass
     public static void beforeClass() throws Exception {
         mockServerAPI = new DefaultMockServerAPI();
-        MockServer.INSTANCE.run(mockServerAPI, "/home/kamil/workspace/sealights/projects/sealights-gradle-plugin-tests/tests");
+        MockServer.INSTANCE.run(mockServerAPI, ".");
+        // TODO recommend latest version?
+        mockServerAPI.setRecommendedVersion(Component.BUILD_SCANNER_COMPONENT_NAME, "3.0.1720", "https://agents.sealights.co/sealights-java/sealights-java-3.0.1720.zip");
         token = MockServer.INSTANCE.getToken(mockServerAPI);
     }
 
@@ -41,16 +63,30 @@ public class BuildScannerTests {
             setGradleVersion(gradleVersion);
             executeBuild(projectName, "buildNameAbc");
 
-            List<Object> agentEvents = mockServerAPI.getBuildMaps();
-            System.out.println();
+            List<DefaultMockServerAPI.BuildMapping> agentEvents = mockServerAPI.getBuildMappings();
+
+            // TODO add nice assertions
+            List<String> methodNames = agentEvents.stream()
+                    .flatMap(event -> convertToBuildMapping(event).getFiles().stream())
+                    .flatMap(file -> file.getMethods().stream())
+                    .map(HashedMethodData::getDisplayName)
+                    .collect(Collectors.toList());
+
+            assertThat(methodNames).containsExactly("public int add(int, int)", "public JavaOnlyExampleClass()");
         }
+    }
+
+    @SneakyThrows
+    private BuildMappingRequest convertToBuildMapping(DefaultMockServerAPI.BuildMapping rawBuildMapping) {
+        return objectMapper.readValue(objectMapper.writeValueAsString(rawBuildMapping), BuildMappingRequest.class);
     }
 
     private void executeBuild(String projectName, String buildName) throws IOException, InterruptedException {
         executeGradleCommand(Arrays.asList(
                 String.format(":%s:%s", projectName, "clean"),
                 String.format(":%s:%s", projectName, "build"),
-                String.format("-DslToken=%s -DbuildName=%s", token, buildName))
+                String.format("-DslToken=%s", token),
+                String.format("-DbuildName=%s", buildName))
         );
 
     }
@@ -61,7 +97,8 @@ public class BuildScannerTests {
 
     private String executeGradleCommand(List<String> command) throws IOException, InterruptedException {
         File projectDirFile = new File(TESTS_PROJECTS_DIR);
-        String gradlew = new File(projectDirFile, "gradlew").getAbsolutePath();
+        String gradlewExecutableName = OsDetector.isWindows() ? "gradlew.bat" : "gradlew";
+        String gradlew = new File(projectDirFile, gradlewExecutableName).getAbsolutePath();
         ArrayList<String> finalCommand = new ArrayList<>();
         finalCommand.add(gradlew);
         finalCommand.addAll(command);
